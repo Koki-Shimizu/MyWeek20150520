74375906bcf3976436847f93c6140c484e11bfa7
Koki <u2daijibrother@gmail.com>
2015-05-20 05:12:48 +0900

    60と70のメソッドを追加まで

--- MyWeek/Program.cs
+++ MyWeek/Program.cs
@@ -14,7 +14,10 @@ namespace MyWeek
             var da = new DatabaseAccess();
 
             // DBにアクセスして、従業員番号60の人の名前を取得する。
-            var name = da.GetEmployeeNameByEmployeesIdIs60();
+            Console.WriteLine(da.GetEmployeeNameByEmployeesIdIs60());
+
+            // DBにアクセスして、従業員番号70の人の名前を取得する。
+            Console.WriteLine(da.GetEmployeeNameByEmployeesIdIs70());
         }
     }
 
@@ -40,9 +43,31 @@ namespace MyWeek
                 }
             }
             return string.Empty;
-
         }
 
+
+
+
+        public string GetEmployeeNameByEmployeesIdIs70()
+        {
+            var connect = "User Id=hr; password=hr;" + "Data Source=localhost:1521/orcl; Pooling=false;";
+
+            using (var con = new OracleConnection())
+            {
+                con.ConnectionString = connect;
+                con.Open();
+
+                var cmd = con.CreateCommand();
+                cmd.CommandText = "select name from Employees where employees_id = 70";
+
+                var reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    return reader.GetString(0);
+                }
+            }
+            return string.Empty;
+        }
     }
 
    
